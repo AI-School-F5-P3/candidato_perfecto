4ef948ea7dbd0d7311d98e4856a8b45e2cbdd0b1
SergioSierraMelero <sergiosm365@gmail.com>
2025-02-13 12:28:31 +0100

    borro

--- candidato_perfecto/src/frontend/__init__.py
+++
@@ -1,2 +0,0 @@
-# File: /candidato_perfecto/candidato_perfecto/src/frontend/__init__.py
-from .ui import load_custom_css, setup_page_config, create_weight_sliders, create_main_sections, display_ranking
